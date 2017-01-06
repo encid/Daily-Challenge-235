af98065381f09e1b1ce494562384533c04c2f52f
encid <ryancav@gmail.com>
2017-01-06 15:28:05 -0500

    commit. made so it loops

--- Daily-Challenge-235/Program.cs
+++ Daily-Challenge-235/Program.cs
@@ -9,24 +9,26 @@ namespace DailyChallenge235
     {
         static void Main()
         {
-            int a, b;
-            string[] str;
+            while (true) {
+                int a, b;
+                string[] str;
 
-            Console.WriteLine("Ruth-Aaron pair checker");
-            Console.Write("Enter two consecuitive numbers\nseparated by commas (i.e. '601,602'): ");
-            var input = Console.ReadLine();
+                Console.WriteLine("Ruth-Aaron pair checker");
+                Console.Write("Enter two consecuitive numbers\nseparated by commas (i.e. '601,602'): ");
+                var input = Console.ReadLine();
 
-            str = input.Split(',');
-            a = int.Parse(str[0]);
-            b = int.Parse(str[1]);
+                str = input.Split(',');
+                a = int.Parse(str[0]);
+                b = int.Parse(str[1]);
 
-            if (IsRuthAaron(a, b)) {
-                Console.WriteLine("({0},{1}) VALID", a, b);
+                if (IsRuthAaron(a, b)) {
+                    Console.WriteLine("({0},{1}) VALID", a, b);
+                }
+                else
+                    Console.WriteLine("({0},{1}) NOT VALID", a, b); 
             }
-            else
-                Console.WriteLine("({0},{1}) NOT VALID", a, b);
 
-            Console.ReadKey();
+            //Console.ReadKey();
         }
 
         public static List<int> GetPrimeFactors(int n)
